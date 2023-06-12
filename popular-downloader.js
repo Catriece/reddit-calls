b3084bb5cf44faa8a37b56e93f36b8f81a1c477c
Catriece Gilbert <119451056+Catriece@users.noreply.github.com>
2023-06-11 20:16:36 -0400

    My attempt at completing the advanced portion of the exercise

--- popular-downloader.js
+++ popular-downloader.js
@@ -4,21 +4,22 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "./downloads");
 
-writeFile(filePath, data, (err) => {
-  if (err) return console.error(err);
-});
-
 fetch("https://reddit.com/r/programmingHumor.json")
   .then((res) => res.json())
   .then(({ data: { children } }) => {
-    console.log(children);
-    const articles = [];
+    console.log("children", children);
 
-    for (let article of children) {
-      articles.push({
-        url: article.data.url,
-        title: article.data.title,
-        author: article.data.author,
-      });
+    for (let media of children) {
+      if (
+        media.data.url.endsWith(".jpg") ||
+        media.data.url.endsWith(".png") ||
+        media.data.url.endsWith(".gif")
+      ) {
+        writeFile(filePath, JSON.stringify(media), (err) => {
+          if (err) return console.error(err);
+          else console.log("New File Added to Downloads");
+        });
+      }
     }
-  });
+  })
+  .catch((err) => console.error(err));
